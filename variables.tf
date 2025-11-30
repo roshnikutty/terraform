9af39700f7c9f670e27dc0cbb5d856c85051977a
roshnikutty <roshni.ajkutty@gmail.com>
2025-11-30 09:48:27 -0500

    declare vars

--- variables.tf
+++ variables.tf
@@ -4,3 +4,13 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name for Snowpipe data"
+  type        = string
+}
+
+variable "s3_prefix" {
+  description = "S3 prefix/folder path for data files"
+  type        = string
+}
